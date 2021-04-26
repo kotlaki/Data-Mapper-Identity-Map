591058885c669b7b986e095e8d5f9e30eb2a45f4
kotlaki <voland171@mail.ru>
2021-04-26 15:53:10 +0500

    Add Data Mapper & Identity Map Patterns.

--- src/main/java/org/kurganov/datamapper/InfoMapper.java
+++ src/main/java/org/kurganov/datamapper/InfoMapper.java
@@ -4,4 +4,5 @@ import java.util.List;
 
 public interface InfoMapper {
     List<Info> findAll();
+    Info findById(Integer id);
 }
--- src/main/java/org/kurganov/datamapper/InfoMapperImpl.java
+++ src/main/java/org/kurganov/datamapper/InfoMapperImpl.java
@@ -11,8 +11,24 @@ public class InfoMapperImpl implements InfoMapper {
     @Autowired
     private InfoRepo infoRepo;
 
+    @Autowired
+    private MyIdentity buffer;
+
     @Override
     public List<Info> findAll() {
         return infoRepo.findAll();
+    }
+
+    @Override
+    public Info findById(Integer id) {
+        if (buffer.getInfoMap().containsKey(id)) {
+            System.out.println("Значение найдено! Возвращаем из Map`ы!");
+            return buffer.getInfo(id);
+        } else {
+            buffer.addInfo(infoRepo.findById(id).get());
+            System.out.println("Значение не найдено! Делаем обращение к БД и помещаем в Map`у!");
+            System.out.println("Вернули из Map`ы! " + buffer.getInfo(id).getName());
+            return buffer.getInfo(id);
+        }
     }
 }
